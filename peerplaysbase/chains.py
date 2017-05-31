3efe5525b13a9e04a2883d285bd5f64394cebcef
Aaron Cox <aaron.cox@greymass.com>
2017-05-31 18:01:42 -0400

    Testnet 3

--- peerplaysbase/chains.py
+++ peerplaysbase/chains.py
@@ -1,6 +1,6 @@
 known_chains = {
     "PPY1": {
-        "chain_id": "f63221f375c735b4cfc9b30be11bb3466808ee796ccc9c5311f0446c4c527767",
+        "chain_id": "32a70325bb61da8369af93a1898b9da5ba44180fb07204bbda3fa9ec30ef7eb3",
         "core_symbol": "PPY",
         "prefix": "PPY"},
 }
